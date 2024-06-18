36b2855397ce5ba9726bf89de58c7b5fecd527f7
lmutsuha79 <lmutsuha79@gmail.com>
2024-06-18 17:20:49 +0100

    add brands and products view sets and routes

--- DRF_Ecommerce/apps/products/views.py
+++ DRF_Ecommerce/apps/products/views.py
@@ -19,3 +19,27 @@ class CategoryViewSet(viewsets.ViewSet):
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for viewing the brands.
+    """
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProductViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for viewing the products.
+    """
+    queryset = Product.objects.all()
+
+    @extend_schema(responses=ProductSerializer)
+    def list(self, request):
+        serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
